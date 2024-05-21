c4d42b60daeba6b6c5a2f0b5e5fecae578165eed
GianMossimo <saccomannogian@gmail.com>
2024-05-20 20:53:05 -0400

    Fixed Anchor Tags

--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -5,14 +5,14 @@ import { faHtml5, faCss3Alt, faJs, faReact } from '@fortawesome/free-brands-svg-
 
 const Portfolio = () => {
   return (
-    <div className='port__portfolio-container' id='lang' >
+    <div className='port__portfolio-container' id='portfolio' >
       <h1>My Language Stack</h1>
 
       <div className='port__portfolio-container_icons'>
-        <a title='HTML5'><FontAwesomeIcon className='fab' icon={faHtml5} /></a>
-        <a title='CSS'><FontAwesomeIcon className='fab' icon={faCss3Alt} /></a>
-        <a title='JavaScript'><FontAwesomeIcon className='fab' icon={faJs} /></a>
-        <a title='React'><FontAwesomeIcon className='fab' icon={faReact} /></a>
+        <a href='#portfolio' title='HTML5'><FontAwesomeIcon className='fab' icon={faHtml5} /></a>
+        <a href='#portfolio' title='CSS'><FontAwesomeIcon className='fab' icon={faCss3Alt} /></a>
+        <a href='#portfolio' title='JavaScript'><FontAwesomeIcon className='fab' icon={faJs} /></a>
+        <a href='#portfolio' title='React'><FontAwesomeIcon className='fab' icon={faReact} /></a>
       </div>
     </div>
   )
